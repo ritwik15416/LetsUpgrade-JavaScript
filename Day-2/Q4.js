f4413e160bb8d07051bdc49f8d4f12e67ed0bc2e
Ritwik Jha <64138168+ritwik15416@users.noreply.github.com>
2020-08-31 12:00:23 +0530

    Create Q4.js

---
+++ Day-2/Q4.js
@@ -0,0 +1,10 @@
+// Input array of strings
+let arr = ['Happy','birthday','to','you']
+// Main program
+for(let i=0;i<arr.length;i++){
+  for(let j=0;j<arr[i].length;j++){
+    if(arr[i][j]==='a'){
+    console.log(arr[i]);
+  }
+  }
+}
