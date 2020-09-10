72d8d237832f40bdd62a6b836a7ed057ef8f4722
Ritwik Jha <64138168+ritwik15416@users.noreply.github.com>
2020-09-10 11:06:39 +0530

    Create Q3.js

---
+++ Day-4/Q3.js
@@ -0,0 +1,11 @@
+// Array of objects 
+const profile = [{'name':'abc','age':20,'country':'India','hobbies':['badminton','reading books']},
+{'name':'def','age':22,'country':'USA','hobbies':['badminton','watching movies']},
+{'name':'ghi','age':30,'country':'UK','hobbies':['football','reading books']},
+{'name':'lmn','age':40,'country':'India','hobbies':['badminton','reading books']},
+{'name':'jkl','age':28,'country':'India','hobbies':['football','reading comics']}]
+
+// Prints all the objects in the array
+for(let i=0;i<profile.length;++i){
+	console.log(profile[i]);
+}
